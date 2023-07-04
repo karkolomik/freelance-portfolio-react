7fc26862225be20dd3a739b1cf8325c68397075a
karkolomik <karkolom@gmail.com>
2023-07-04 14:29:51 +0300

    CV update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -12,14 +12,21 @@ const Header = () => {
         </h1>
         <div className="header__text">
           <p>
-            I am a Frontend developer with passion of transforming visions into
-            pixel-perfect realities through innovative solutions
+            I am a Front-end React developer specializing in building responsive
+            and interactive web applications. Currently seeking an entry-level
+            position. Proficient in HTML, CSS, and JavaScript. Use React to
+            create dynamic user interfaces that provide great user experiences.
           </p>
         </div>
         <div className="header__photo">
           <img src={photo} alt="profile_photo" />
         </div>
-        <a href="#!" className="btn">
+        <a
+          href="https://drive.google.com/file/d/1XaxvLRLK0FdW4owFez8YbuJ_XDfohnwC/view?usp=sharing"
+          className="btn"
+          target="_blank"
+          rel="noreferrer"
+        >
           Download CV
         </a>
       </div>
